6ede9fda0e7bb2fa633f2f091fd1ed3af74d6b32
juliag4 <159590870+juliag4@users.noreply.github.com>
2026-04-22 20:04:49 -0400

    Update Root.js

--- src/Root.js
+++ src/Root.js
@@ -5,12 +5,13 @@
 class Root extends HTMLElement{
   constructor(){
     super();
-
+    console.log("before html");
     this.innerHTML = `
       <div class="header">
         Initial header class
       </div>
     `;
+    console.log("after html");
   }
 }
 customElements.define('root', Root);
