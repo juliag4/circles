55b332dae8225a0668489efa76980204b5b58f17
juliag4 <julgum@proton.me>
2026-04-26 20:51:39 -0400

    fixed alignment in root.js

--- src/Root.js
+++ src/Root.js
@@ -1,63 +1,63 @@
 import {AddStyle} from './Styles.js';
 
 AddStyle(`
-  circle-root{
-    display: flex;
-    width: 100vw;
-    height: 100vh;
-    margin: 0;
-    font-family: sans-serif;
-  }
+    circle-root{
+        display: flex;
+        width: 100vw;
+        height: 100vh;
+        margin: 0;
+        font-family: sans-serif;
+    }
 
-  circle-root .name-input-view{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-  }
+    circle-root .name-input-view{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+    }
 
-  circle-root .header{
-    display: flex;
-    color: steelblue;
-    font-size: 50px;
-    font-weight: bold;
-    position: absolute;
-    top: 0;
-  }
-  
-  circle-root .input-button-container{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 40px;
-  }
+    circle-root .header{
+        display: flex;
+        color: steelblue;
+        font-size: 50px;
+        font-weight: bold;
+        position: absolute;
+        top: 0;
+    }
 
-  circle-root .input-button{
-    padding: 5px;
-    border: 1px solid black;
-    border-radius: 3px;
-    font-size: 18px;
-  }
+    circle-root .input-button-container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 40px;
+    }
 
-  circle-root .username{
-    display: flex;
-    background-color: lightgrey;
-  }
+    circle-root .input-button{
+        padding: 5px;
+        border: 1px solid black;
+        border-radius: 3px;
+        font-size: 18px;
+    }
 
-  circle-root .play{
-    display: flex;
-    justify-content: center;
-    width: 50%;
-    background-color: steelblue;
-    color: white;
-    cursor: pointer;
-  }
+    circle-root .username{
+        display: flex;
+        background-color: lightgrey;
+    }
 
-  circle-root .play:hover{
-    filter: brightness(0.7);
-  }
+    circle-root .play{
+        display: flex;
+        justify-content: center;
+        width: 50%;
+        background-color: steelblue;
+        color: white;
+        cursor: pointer;
+    }
+
+    circle-root .play:hover{
+        filter: brightness(0.7);
+    }
 `);
 
 class Root extends HTMLElement{
